b525460e6bad202c66a49a910cbde73ac0e118e7
Jakki12 <jakkesprinter@web.de>
2024-02-21 13:35:26 +0100

    moving balls

--- js/main.js
+++ js/main.js
@@ -6,3 +6,53 @@ window.onload = () => {
              .register('./sw.js');
   }
 }
+
+
+
+let intervalID;
+
+function moveOnRepeat() {
+  intervalID = setInterval(moveit, 30);
+}
+
+function moveit() {
+    var xpos = document.getElementById("mycircle").cx.baseVal.value;
+    console.log(xpos);
+    
+    if(xpos >= 100)
+    {
+        xpos = 0;      
+    }
+    else
+    {
+        xpos += 2;   
+    }
+    
+    document.getElementById("mycircle").cx.baseVal.value = xpos;
+    
+};
+
+
+
+
+function moveOnRepeat2() {
+  setInterval(moveit2, 80);
+}
+function moveit2() {
+    var xpos = document.getElementById("mycircle2").cx.baseVal.value;
+    console.log(xpos);
+    
+    if(xpos >= 300)
+    {
+        xpos = 0;      
+    }
+    else
+    {
+        xpos += 2;   
+    }
+    
+    document.getElementById("mycircle2").cx.baseVal.value = xpos;
+    
+};
+
+
